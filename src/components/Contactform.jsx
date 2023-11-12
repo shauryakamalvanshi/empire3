075a38813e47fa46c70b095e57e6a4ada27485d5
shaurya kamalvanshi <kamalvanshishaurya@gmail.com>
2023-11-12 21:56:20 +0530

    second commit

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -27,10 +27,7 @@ export default function ContactForm() {
       >
         <div
           className="relative left-1/2 -z-10  bg-red-800"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
+         
         />
       </div>
       <div className="mx-auto max-w-2xl text-center">
@@ -165,9 +162,9 @@ setFname(e.target.value);
             </div>
             <Switch.Label className="text-sm leading-6 text-gray-600">
               By selecting this, you agree to our{' '}
-              <a href="#" className="font-semibold text-indigo-600">
+              <div className="font-semibold text-indigo-600">
                 privacy&nbsp;policy
-              </a>
+              </div>
               .
             </Switch.Label>
           </Switch.Group>
