b50eca9d607867dd54cec143e76d0c4d918cc862
shaurya kamalvanshi <kamalvanshishaurya@gmail.com>
2023-11-17 08:59:55 +0530

    13 commit

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -12,21 +12,21 @@ const Hero = () => {
       <div className="relative  flex justify-evenly pt-24 max-lg:flex max-lg:flex-col bg-gradient-to-t from-cyan-200 to-cyan-400 max-h-full  ">
         <div className="flex flex-col p-8 max-lg:flex max-lg:justify-center max-lg:items-center">
             <h1 className="flex flex-col text-6xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl mb-10">
-              <span className="text-6xl bold leading-none font-bebas-neue max-md:text-5xl max-[466px]:text-4xl"> Turning your </span> 
-                <span className="text-4xl max-md:text-2xl">ideas into online</span>
-                <span className="text-9xl max-md:text-7xl">realities</span>  
+              <span className="pb-2 text-6xl bold leading-none font-bebas-neue max-md:text-5xl max-[466px]:text-4xl"> Turning your </span> 
+                <span className="pb-2 text-4xl max-md:text-2xl">ideas into online</span>
+                <span className="text-9xl max-md:text-7xl max-sm:text-5xl">realities</span>  
             </h1>
             <p className=" max-w-[480px] text-2xl text-body-color text-slate-600 font-mono text-2xl mb-10 max-sm:text-lg">
                 Code,design,launch,we do it all
                 </p>
                 <div className="flex p-auto p-auto mt-8">
                   <Link href={"/contact"}>
-        <div className="px-5 py-3 mr-5 text-white uppercase bg-pink-500 border-2 border-transparent rounded-lg text-lg hover:bg-pink-400">
-          Contact us
+        <div className="px-3 py-2 mr-5 text-white uppercase bg-pink-500 border-2 border-transparent rounded-lg text-lg hover:bg-pink-400 max-sm:text-sm">
+          Contact_us
         </div>
         </Link>
-        <div className="px-5 py-3 text-pink-500 uppercase bg-transparent border-2 border-pink-500 rounded-lg dark:text-white hover:bg-pink-500 hover:text-white text-lg">
-          Read more
+        <div className=" px-3 py-2 text-pink-500 uppercase bg-transparent border-2 border-pink-500 rounded-lg dark:text-white hover:bg-pink-500 hover:text-white text-lg">
+          <span className="flex ">Read_more</span>
         </div>
       </div>
          
